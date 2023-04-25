1641d5b093c4258c6ddcf5d6b442d9c0e9ec0598
Enova <17408285+Enovale@users.noreply.github.com>
2023-04-25 12:07:11 -0500

    Add crappy addressable dump shortcut

--- RobotDracula/PluginBootstrap.cs
+++ RobotDracula/PluginBootstrap.cs
@@ -1,7 +1,13 @@
 using System;
+using System.IO;
+using Il2CppInterop.Runtime;
+using Il2CppSystem.Collections.Generic;
+using Il2CppSystem.Linq;
 using RobotDracula.Trainer;
 using RobotDracula.UI;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceLocations;
 
 namespace RobotDracula
 {
@@ -14,6 +20,50 @@ namespace RobotDracula
         {
             if (Input.GetKeyDown(KeyCode.F8))
                 Plugin.ShowTrainer = !Plugin.ShowTrainer;
+            else if (Input.GetKeyDown(KeyCode.F9))
+            {
+                foreach (var resourceLocator in Addressables.ResourceLocators.ToList())
+                {
+                    foreach (var key in resourceLocator.Keys.ToList())
+                    {
+                        var keyStr = key.ToString();
+                        if (keyStr.Length != "01f09278f4396ee4798db52eac694b36".Length ||
+                            keyStr.StartsWith("Assets") || keyStr.Contains("."))
+                        {
+                            var path = "./Export/" + keyStr;
+
+                            try
+                            {
+                                var dir = Path.GetDirectoryName(path);
+                                if (!Directory.Exists(dir))
+                                    Directory.CreateDirectory(dir);
+
+                                var res = new List<IResourceLocation>(Addressables
+                                    .LoadResourceLocationsAsync(keyStr).WaitForCompletion()
+                                    .Cast<IEnumerable<IResourceLocation>>());
+                                foreach (var rl in res)
+                                {
+                                    if (rl.ResourceType == Il2CppType.Of<TextAsset>())
+                                        File.WriteAllBytes(path,
+                                            Addressables.LoadAsset<TextAsset>(keyStr).WaitForCompletion().bytes);
+                                    else if (rl.ResourceType == Il2CppType.Of<Sprite>())
+                                        File.WriteAllBytes(path,
+                                            ImageConversion.EncodeToPNG(Addressables.LoadAsset<Sprite>(keyStr)
+                                                .WaitForCompletion().texture));
+                                    else if (rl.ResourceType == Il2CppType.Of<Texture2D>())
+                                        File.WriteAllBytes(path,
+                                            ImageConversion.EncodeToPNG(Addressables.LoadAsset<Texture2D>(keyStr)
+                                                .WaitForCompletion()));
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Plugin.PluginLog.LogError(e);
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         // Run in fixed update because we don't need frame by frame updates
@@ -21,8 +71,8 @@ namespace RobotDracula
         {
             if (ReactiveUIEnabled && Plugin.ShowTrainer)
                 UiHelper.Update();
-            
-            if(TrainerEnabled)
+
+            if (TrainerEnabled)
                 TrainerManager.Update();
         }
     }
